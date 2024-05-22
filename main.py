c508069eea7a9b4629eda13b6f8d229644d918e9
alkesh <118254569+alkesh6394@users.noreply.github.com>
2024-05-22 11:41:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def sum_of_two_numbers(num1, num2):  # added num2 as a parameter
     return num1 + num2
 
 # Define numbers
-num1 = 100
-num2 = 900
+num1 = 1000
+num2 = 9000
 
 # Calculate sum using the function
 result = sum_of_two_numbers(num1)
